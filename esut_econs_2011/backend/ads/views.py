13bbd969bb111c9291d902dc2a097c1738920c1d
Rufus <likeness14@gmail.com>
2025-07-25 19:27:04 +0100

    created views.py files under ads

--- esut_econs_2011/backend/ads/views.py
+++ esut_econs_2011/backend/ads/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets # type: ignore
+from .models import Ad
+from .serializers import AdSerializer
+
+class AdViewSet(viewsets.ModelViewSet):
+    queryset = Ad.objects.all().order_by('-created_at')
+    serializer_class = AdSerializer
+
